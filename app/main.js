ead7a0cc84d17a84e24aa6b99d0bbf076f9c269a
Hilary Winn <hilary@fuigo.io>
2016-07-05 17:43:12 -0400

    pipes, methods, and directives

--- app/main.js
+++ app/main.js
@@ -13,22 +13,39 @@ var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var core_1 = require('@angular/core');
 // helps create components (decorator + class)
 // component decorator code goes here
-// use `stuff` to create multiline template strings 
+// use `backticks` to create multiline template strings 
 var AppComponent = (function () {
     function AppComponent() {
         // don't use var or let to declare class properties
         this.title = 'Ultra Racing';
-        this.carPart = {
-            'id': 1,
-            'name': 'Super Tires',
-            'description': 'These tires are the very best',
-            'inStock': 5
-        };
+        this.date = new Date('2512-07-03T20:00:00');
+        this.carParts = [{
+                'id': 1,
+                'name': 'Super Tires',
+                'description': 'These tires are the very best',
+                'inStock': 5,
+                'price': 4.99
+            },
+            {
+                'id': 2,
+                'name': 'Sunroof',
+                'description': 'Let the heat in!',
+                'inStock': 0,
+                'price': 100.00
+            }];
     }
+    AppComponent.prototype.totalCarParts = function () {
+        var sum = 0;
+        for (var _i = 0, _a = this.carParts; _i < _a.length; _i++) {
+            var carPart = _a[_i];
+            sum += carPart.inStock;
+        }
+        return sum;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>{{title}}</h1>\n\t\t<h2>{{carPart.name}}</h2>\n\t\t<p>{{carPart.description}}</p>\n\t\t<p>{{carPart.inStock}} in Stock</p>"
+            template: "<h1>{{title}}</h1>\n\t\t<h3>{{date | date:'MMM d, y, h:mm a'}}</h3>\n\t\t<p>There are {{totalCarParts()}} total parts in stock.</p>\n\t\t<ul>\n\t\t\t<li *ngFor=\"let carPart of carParts\">\n\t\t\t\t<h2>{{carPart.name | uppercase }}</h2>\n\t\t\t\t<p>{{carPart.description}}</p>\n\t\t\t\t<p>{{carPart.price | currency:'USD':true}}</p>\n\t\t\t\t<p *ngIf=\"carPart.inStock > 0\">{{carPart.inStock}} in Stock</p>\n\t\t\t\t<p *ngIf=\"carPart.inStock === 0\">Out of Stock</p>\n\t\t\t</li>\n\t\t</ul>"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/main.ts
+++ app/main.ts
@@ -5,13 +5,21 @@ import { Component } from '@angular/core';
 
 
 // component decorator code goes here
-// use `stuff` to create multiline template strings 
+// use `backticks` to create multiline template strings 
 @Component({
 	selector: 'my-app',
 	template: `<h1>{{title}}</h1>
-		<h2>{{carPart.name}}</h2>
-		<p>{{carPart.description}}</p>
-		<p>{{carPart.inStock}} in Stock</p>`
+		<h3>{{date | date:'MMM d, y, h:mm a'}}</h3>
+		<p>There are {{totalCarParts()}} total parts in stock.</p>
+		<ul>
+			<li *ngFor="let carPart of carParts">
+				<h2>{{carPart.name | uppercase }}</h2>
+				<p>{{carPart.description}}</p>
+				<p>{{carPart.price | currency:'USD':true}}</p>
+				<p *ngIf="carPart.inStock > 0">{{carPart.inStock}} in Stock</p>
+				<p *ngIf="carPart.inStock === 0">Out of Stock</p>
+			</li>
+		</ul>`
 
 })
 
@@ -19,12 +27,34 @@ import { Component } from '@angular/core';
 class AppComponent {
 	// don't use var or let to declare class properties
 	title = 'Ultra Racing';
-	carPart = {
+
+	date = new Date('2512-07-03T20:00:00');
+
+	carParts = [{
 		'id': 1,
 		'name': 'Super Tires',
 		'description': 'These tires are the very best',
-		'inStock': 5
-	} 
+		'inStock': 5,
+		'price' : 4.99
+	},
+	{
+		'id': 2,
+		'name': 'Sunroof',
+		'description': 'Let the heat in!',
+		'inStock': 0,
+		'price' : 100.00
+	}];
+
+	totalCarParts() {
+		let sum = 0;
+
+		for(let carPart of this.carParts) {
+			sum += carPart.inStock;
+		}
+
+		return sum;
+	}
+
 }
 
 bootstrap(AppComponent)
